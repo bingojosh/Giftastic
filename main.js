bdcdf30099570e3a18b11c04a3073067c958755b
Josh LaRochelle <joshua.r.larochelle@gmail.com>
2018-12-22 14:20:48 -0500

    All Done Bois

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var topics = ["spongebob", "drake", "two buttons", "batman", "patrick", "keanu reeves", "hasselhoff", "wrestler", "Weenie Hut Jr", "chum bucket", "goku", "santa clause"]
+var response=new Object;
 
 $(document).ready(function(){
     
@@ -17,7 +18,7 @@ $(document).ready(function(){
         url += '?' + $.param({
             'api_key': "P8LsoM3TmHKEAB4j59Mh9pCLy69IiqGy",
             'q': q,
-            'limit': "10",
+            'limit': "50",
             'offset' : '0',
             // 'rating': 'G'
         });
@@ -27,31 +28,12 @@ $(document).ready(function(){
             method: 'GET',
         }).done(function(result) {
             console.log(result);
-            for(i=0;i<result.data.length;i++){
-                var img = $("<img>").attr({
-                    "animated": "no",
-                    "data-anim": result.data[i].images.fixed_width.url,
-                    "data-still": result.data[i].images.fixed_width_still.url,
-                    "src": result.data[i].images.fixed_width_still.url,
-                    "id": i
-                })
+            response=result;
+            memePrinter(result, 0);
+            var moreBtn = $("<button>").text("More Memes Please").addClass("moreBtn")
+            moreBtn.attr("i-value", 10)
+            moreBtn.appendTo($("#memes"))
 
-                img.on("click", function(){
-                    if($(this).attr("animated") === "no"){
-                        $(this).attr("animated", "yes")
-                        $(this).attr("src", $(this).attr("data-anim"))
-                    }
-                    else{
-                        $(this).attr("animated", "no")
-                        $(this).attr("src", $(this).attr("data-still"))
-                    }
-                })
-
-                var fig = $("<figure>").addClass("gif")
-                fig.append(img);
-                fig.append(`<figcaption> Rating: ${result.data[i].rating}`)
-                $("#memes").append(fig)
-            }
         }).fail(function(err) {
             throw err;
         });
@@ -60,6 +42,7 @@ $(document).ready(function(){
     $("#meme-submit").on("click", function(event){
         event.preventDefault();
         var newMeme = $("#meme").val().trim();
+        $("#meme").val("");
         console.log(newMeme)
         if(newMeme === ""){
             alert("Not falling for that one.")
@@ -70,6 +53,20 @@ $(document).ready(function(){
             }
         else{     
             alert("That is already an available meme.")
+        }
+    })
+
+    $(document.body).on("click", ".moreBtn", function(){
+        var i=parseInt($(".moreBtn").attr("i-value"))
+        if(i<40){
+        memePrinter(response,i);
+        $(".moreBtn").appendTo($("#memes"))
+        i = i+10
+        $(".moreBtn").attr("i-value", i)
+        }
+        else{
+            $(".moreBtn").remove();
+            alert("That is all of the memes.")
         }
     })
 })
@@ -83,3 +80,32 @@ function buttonPrint(){
         $("#buttons").append(button)
     }
 }
+
+function memePrinter(result, start){
+    for(i=start;i<start+10;i++){
+        var img = $("<img>").attr({
+            "animated": "no",
+            "data-anim": result.data[i].images.fixed_width.url,
+            "data-still": result.data[i].images.fixed_width_still.url,
+            "src": result.data[i].images.fixed_width_still.url,
+            "id": i
+        })
+
+        img.on("click", function(){
+            if($(this).attr("animated") === "no"){
+                $(this).attr("animated", "yes")
+                $(this).attr("src", $(this).attr("data-anim"))
+            }
+            else{
+                $(this).attr("animated", "no")
+                $(this).attr("src", $(this).attr("data-still"))
+            }
+        })
+
+        var fig = $("<figure>").addClass("gif")
+        fig.append(img);
+        fig.append(`<figcaption> Rating: ${result.data[i].rating}`)
+        $("#memes").append(fig)
+    }
+}
+
